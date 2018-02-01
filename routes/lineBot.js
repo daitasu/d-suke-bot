2d7788e353e882e834a5efb61a3f7f1cbfd76473
DaisukeFFF <ifc.dai.faculty@gmail.com>
2018-02-02 01:59:44 +0900

    test

--- routes/lineBot.js
+++ routes/lineBot.js
@@ -45,9 +45,9 @@ bot.on('message', async (event) => {
     console.log('message event');
     let replyMessage;
 
-    if(event.message.text.indexOf('>Dランド')) {
+    if(event.message.text.indexOf('>Dランド') !== -1) {
         replyMessage = await getWaitingTime("land");
-    }else if(event.message.text.indexOf('>Dシー')){
+    }else if(event.message.text.indexOf('>Dシー') !== -1){
         replyMessage = await getWaitingTime("sea");
     }else {
         replyMessage = "待ち時間を取得するには、メニューからボタンをお選び下さい。";
