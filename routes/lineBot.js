67b319109847a287ce268ba4c8395e6a931ab676
DaisukeFFF <ifc.dai.faculty@gmail.com>
2018-02-13 20:56:00 +0900

    change indexOf to test method

--- routes/lineBot.js
+++ routes/lineBot.js
@@ -4,7 +4,6 @@ const router = express.Router();
 const bodyParser = require('body-parser');
 const cheerio = require('cheerio-httpcli');
 
-
 const bot = linebot({
     channelId: process.env.LINE_CHANNEL_ID,
     channelSecret: process.env.LINE_CHANNEL_SECRET,
@@ -64,9 +63,9 @@ async function getWaitingTime(name) {
 
     lists = lists.trim().replace(/\t/g, "").replace(/\n+/g, ",").split(",");
     lists.forEach((list) => {
-        if (list.indexOf("更新") !== -1) {
+        if (list.indexOf("更新") !== -1){
             replyMessage += list;
-        }else if(list.indexOf("FP") !== -1 || list.indexOf("中") !== -1 || list.indexOf("分") !== -1 || list.indexOf("情報なし") !== -1 ||list.indexOf("案内") !== -1 ) {
+        } else if (/"FP"|"中"|"分"|"情報なし"|"案内"/.test(list)){
             replyMessage += "\n" + list;
         } else {
             replyMessage += "\n\n" + list;
